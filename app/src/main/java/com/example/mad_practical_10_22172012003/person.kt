b38108447491ca4fe009fee5715efeb187753b89
surekhabhanvar123 <surekhabenbhanvar12@gmail.com>
2023-10-27 12:29:07 +0530

    hello

--- app/src/main/java/com/example/mad_practical_10_22172012003/person.kt
+++ app/src/main/java/com/example/mad_practical_10_22172012003/person.kt
@@ -1,13 +1,60 @@
 package com.example.mad_practical_10_22172012003
 
+import org.json.JSONObject
 import java.io.Serializable
-import java.net.Inet4Address
 
-class Person(var id:String,
-             var name: String,
-             var emailId:String,
-             var phoneNo:String,
-             var address:String,
-             var latitude:Double,
-             var longitude:Double):Serializable {
+class Person (jsonObject: JSONObject) :Serializable {
+    val name: String = ""
+    val longitude: Double
+        get() {
+            TODO()
+        }
+    val latitude: Double
+        get() {
+            TODO()
+        }
+    var id:String
+    var Name: String
+    var EmailId: String
+    var PhoneNo: String
+    var Address: String
+    var Latitude: Double
+    var Longitude:Double
+    /**
+     * {
+    "id": "62fc930c5831f21801d78da8",
+    "email": "guerra_rodgers@gnu.ac.in",
+    "phone": "+919289635723",
+    "profile": {
+    "name": "Guerra Rodgers",
+    "address": "88 College Place, Umapine, Oregon",
+    "location": {
+    "lat": 35.924816,
+    "long": -85.003777
+    }
+    }
+    }
+     */
+    /*
+    * [
+    * {"_id":"5f8d677c68d8ae7ceab6a732",
+    * "name":{"first":"Lloyd","last":"York"},
+    * "email":"lloyd.york@undefined.net",
+    * "phone":"+1 (817) 545-3660",
+    * "address":"311 Livonia Avenue, Belva, Ohio, 6019"}*/
+    init {
+        id = jsonObject.getString("id")
+        EmailId = jsonObject.getString("email")
+        PhoneNo = jsonObject.getString("phone")
+        val profileJson = jsonObject.getJSONObject("profile")
+        Name = profileJson.getString("name")
+        Address = profileJson.getString("address")
+        val locationJson = profileJson.getJSONObject("location")
+        Latitude = locationJson.getDouble("lat")
+        Longitude = locationJson.getDouble("long")
+    }
+
+    override fun toString(): String {
+        return "$Name\n$PhoneNo\n$EmailId\n$Address\nlat:$Latitude\nlog:$Longitude"
+    }
 }
--- app/src/main/java/com/example/mad_practical_10_22172012003/personAdapter.kt
+++ app/src/main/java/com/example/mad_practical_10_22172012003/personAdapter.kt
@@ -1,18 +1,35 @@
 package com.example.mad_practical_10_22172012003
 
+import android.annotation.SuppressLint
 import android.content.Context
+import android.content.Intent
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ArrayAdapter
+import android.widget.Button
+import android.widget.TextView
+import com.google.android.material.button.MaterialButton
 import java.util.ArrayList
 
-class personAdapter(context:Context,val array:ArrayList<Person>):ArrayAdapter<Person>(context,0,array)
-{
+class PersonAdapter(context:Context,val array:ArrayList<Person>):ArrayAdapter<Person>(context,0,array) {
+
+
+
+
+    @SuppressLint("MissingInflatedId")
     override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
-        val view=LayoutInflater.from(parent.context).inflate(R.layout.person_item,parent,false)
+
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.Person_item,parent,false)
+        val person_data = array.get(position)
+        view.findViewById<TextView>(R.id.textView_name).text=person_data.Name
+        view.findViewById<TextView>(R.id.textView_no).text=person_data.PhoneNo
+        view.findViewById<TextView>(R.id.textView_email).text=person_data.EmailId
+        view.findViewById<TextView>(R.id.textView_address).text=person_data.Address
+        view.findViewById<MaterialButton>(R.id.btn_location).setOnClickListener {
+            Intent(parent.context,MapsActivity::class.java).putExtra("Object",person_data).apply { context.startActivity(this) }
+        }
         return view
     }
-    val person data=array.get(position)
-            view findViewbyid<textview>(R.id.name).text=person data.email
+
 }
